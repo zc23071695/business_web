07623d20903ae56acd87bd407957f34d85296812
zc23071695 <zc23071695@outlook.com>
2019-03-23 10:53:31 +0800

    新增大量的文件

--- app/js/api.js
+++ app/js/api.js
@@ -1,5 +1,14 @@
-var host = 'http://10.36.18.25:7777/'
-// var host = '';
+var host = 'http://10.36.145.245:7777/server/';
+//username password 数据格式json字符串  POST请求
 var obj = {
-    login: host + 'a.php',
+    login: host + 'login.php',
+    register: host + 'register.php',
+    check_username: host + 'check_username.php'
 }
+
+
+
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// gulp.task('default', ['allFile','server', 'watch', 'sass']);
+
+gulp.task('allFile', function () {
+    gulp.src(['app/**/*', '!app/sass/**/*'])
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
+})
+
+gulp.task('watch', function () {
+    gulp.watch(['app/**/*', '!app/sass/**/*'], ['allFile']);
+    gulp.watch('app/sass/**/*.scss', ['sass']);
+
+})
+gulp.task('server', function () {
+    connect.server({
+        root: 'dist', //设置根目录
+        livereload: true, // 是否热更新
+        port: 7733
+    });
+})
+
+
+
+gulp.task('default', function () {
+    // 将你的默认的任务代码放在这
+});
+
+gulp.task('mod', function () {
+    // 做一些事
+    var options = {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyJS: true,
+        minifyCSS: true
+    };
+    gulp.src('*.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('modify'));
+
+});
+
+gulp.task("cssmin", function () {
+    var cssmin = require('gulp-cssmin');
+    var rename = require('gulp-rename');
+
+    gulp.src('*.css')
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('modify'));
+})
+
+const babel = require('gulp-babel');
+
+gulp.task('babel', () => {
+    gulp.src('test.js')
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(gulp.dest('dist'))
+}
+
+);
