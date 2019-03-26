48c1df53d9a095f33849cc4029030c3968d13499
zc23071695 <zc23071695@outlook.com>
2019-03-26 19:08:52 +0800

    cookie

--- dist/js/search.js
+++ dist/js/search.js
@@ -1,8 +1,8 @@
 const baidu = (function () {
-    let $input,ulBox,box;
+    let $input, ulBox, box;
     return {
         init(ele) {
-         
+
             $input = document.querySelector("#skey");
             $inputButton = document.querySelector("#btnHeaderSearch");
             ulBox = document.querySelector(".show");
@@ -15,30 +15,30 @@ const baidu = (function () {
             }
             $input.oninput = function () {
                 console.log(this.value)
-                if(this.value.trim() == ''){
+                if (this.value.trim() == '') {
                     self.hidden();
-                }else{
+                } else {
                     self.getData(this.value);
                     self.show();
                 }
             }
-            $input.onclick = function(e) {
+            $input.onclick = function (e) {
                 e.stopPropagation();
             }
             document.onclick = function () {
                 self.hidden();
             }
-            $inputButton.onclick =function(){
-                location.href="html/test.html";
+            $inputButton.onclick = function () {
+                location.href = "html/search.html";
             }
             ulBox.onclick = function (e) {
-                if(e.target.nodeName === 'LI') {
-                    $input.value = e.target.innerHTML;   
+                if (e.target.nodeName === 'LI') {
+                    $input.value = e.target.innerHTML;
                 }
             }
         },
         show() {
-            if($input.value.trim()!= '')
+            if ($input.value.trim() != '')
                 ulBox.style.display = "block"
         },
         hidden() {
@@ -50,7 +50,7 @@ const baidu = (function () {
                 wd: val,
                 cb: "baidu.insertData"
             }
-            sendJsonp(url,data);
+            sendJsonp(url, data);
         },
         insertData(data) {
             ulBox.innerHTML = '';
@@ -58,7 +58,7 @@ const baidu = (function () {
                 const li = document.createElement('li');
                 li.innerHTML = x;
                 ulBox.appendChild(li);
-                
+
             })
         }
     }
